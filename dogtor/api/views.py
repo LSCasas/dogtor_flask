c60ed56fccc28d5bd59e86edca36c918b3ae55be
Luis Casas <luisscasas45@gmail.com>
2025-03-14 16:30:23 -0600

    FEAT/ add PUT and DELETE methods for species

--- dogtor/api/views.py
+++ dogtor/api/views.py
@@ -42,15 +42,36 @@ def owners():
 def procedures():  
     return []
 
-@api_blueprint.route("/species/", methods=["GET", "POST"])
-def species():
-    if request.method == "GET":
-        species_all = models.Species.query.all()
-        return [{"id": species.id, "name": species.name} for species in species_all]
 
-    if request.method == "POST":
-        data = request.get_json()
-        species_instance = models.Species(name=data['name'])
-        db.session.add(species_instance)
-        db.session.commit()
-        return {"detail": f"species{species_instance.name} created successfully"}
+@api_blueprint.route("/species/<int:species_id>", methods=["GET", "PUT", "DELETE"])
+@api_blueprint.route("/species/", methods=["GET", "POST"])
+def species(species_id=None):
+    if species_id is not None:
+        if request.method == "GET":
+            species = models.Species.query.get_or_404(species_id)
+            return {"id": species.id, "name": species.name}
+        
+        elif request.method == "PUT":
+            data = request.get_json()
+            species = models.Species.query.get_or_404(species_id)
+            species.name = data['name']
+            db.session.commit()
+            return {"detail": f"Species {species.name} updated successfully"}
+        
+        elif request.method == "DELETE":
+            species = models.Species.query.get_or_404(species_id)
+            db.session.delete(species)
+            db.session.commit()
+            return {"detail": f"Species {species.name} deleted successfully"}
+
+    else:
+        if request.method == "GET":
+            species_all = models.Species.query.all()
+            return [{"id": species.id, "name": species.name} for species in species_all]
+
+        elif request.method == "POST":
+            data = request.get_json()
+            species_instance = models.Species(name=data['name'])
+            db.session.add(species_instance)
+            db.session.commit()
+            return {"detail": f"Species {species_instance.name} created successfully"}
